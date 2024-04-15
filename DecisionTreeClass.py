5862d2c4f74247cec6cb190a94478baabd5d0291
ArijeetInfomaticae <arijeetsinha2@gmail.com>
2024-04-15 07:56:06 +0530

    Dataset File Path updated

--- DecisionTreeClass.py
+++ DecisionTreeClass.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import r2_score
 
-data = pd.read_csv('music.csv')
+data = pd.read_csv('dataset/music.csv')
 
 X = data.drop(columns=['genre'])
 y = data['genre']
@@ -12,6 +12,6 @@ x_train , x_test, y_train, y_test = train_test_split(X , y, test_size=0.1)
 model = DecisionTreeClassifier()
 model.fit(x_train, y_train)
 predictions = model.predict(x_test)
-score = accuracy_score(y_test, predictions)
+score = r2_score(y_test, predictions)
 print(predictions)
 print(score)
